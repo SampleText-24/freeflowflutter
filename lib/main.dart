8270f6f9124ca1f9e747a866dd0f2a6f2c8947d1
Alexey Sokolov <Sample24Text@gmail.com>
2023-04-01 21:41:34 +0700

    Приветики-пистолетики

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,22 @@
-void main(List<String> arguments) {
-  print('privet kak dela?');
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ny zdarova!',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello world!')
+        ),
+        body: const Center(
+          child: Text('Привет мир', style: TextStyle(color: Colors.pinkAccent)),
+        ),
+      ),
+    );
+  }
 }
